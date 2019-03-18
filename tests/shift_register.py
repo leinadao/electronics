d447852a306f0a8f93a98112980b3e44b681c1fd
Daniel Home <danielhome@Daniels-MacBook-Pro.local>
2019-03-18 01:02:38 +0000

    Add random flashing test function for a shift register.

--- tests/shift_register.py
+++ tests/shift_register.py
@@ -1,3 +1,9 @@
+from random import getrandbits as random_getrandbits
+from time import (
+	sleep as time_sleep,
+	time as time_time,
+)
+
 from ..components import ShiftRegister
 
 ## Create a test shift register on pin 4 and 18:
@@ -7,3 +13,16 @@ test_shift_register = ShiftRegister (
 	clock_pin_id = 17,
 	latch_pin_id = 18,
 )
+
+def flash_random (shift_register, iterations, pause_seconds):
+	'''
+		Iterate through the given number of random
+		output configurations, pausing for the given
+		number of seconds inbetween each, using
+		the given shift register.
+	'''
+	for i in range (iterations):
+		shift_register.from_list (
+			[random_getrandbits (1) for a in range (shift_register.number_outputs)],
+		)
+		time_sleep (pause_seconds)
