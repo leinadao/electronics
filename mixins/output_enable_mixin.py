d12f4b34725c9ac5927a623a862f255c28bc0378
Daniel Home <danielhome@daniels-mbp.home>
2019-03-26 08:20:35 +0000

    Update OutputEnableMixin to ensure it's enabled on initialisation.

--- mixins/output_enable_mixin.py
+++ mixins/output_enable_mixin.py
@@ -17,7 +17,8 @@ class OutputEnableMixin ():
 		## Ensure output is enabled if enable pin used:
 		if self.controlling_enable_pin:
 			GPIO.setup (self.__enable_pin_id, GPIO.OUT)
-			self.__enable_value = self.OFF if self.enable_active_low else self.ON
+			self.__enable_value = self.ON if self.enable_active_low else self.OFF
+			self.enable ()
 		super ().__init__ (*args, **kwargs)
 
 	@property
